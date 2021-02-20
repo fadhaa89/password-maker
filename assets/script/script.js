99516b03f39ad14f7a4d77d6c9da5f7114b05615
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-02-20 16:30:07 -0600

    added CSS,stated on script

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+
+const criteria = {
+    numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    lowerLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    upperLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    specials: ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'],
+    userChoices: [],
+    passwordLength: null,
+    lowerChoice: null,
+    upperChoice: null,
+    numbersChoice: null,
+    specialsChoice: null,
+  
+  };
